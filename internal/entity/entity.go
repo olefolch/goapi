66410d05fdb2401f46b69b81607eff6c0488c3a2
Leonardo Folchini <leonardo.folchini@hotmail.com>
2024-02-02 22:10:00 -0300

    feat: inicialização do projeto e criação de entity

---
+++ internal/entity/entity.go
@@ -0,0 +1,35 @@
+package entity
+
+import "github.com/google/uuid"
+
+type Category struct {
+	ID   string
+	Name string
+}
+
+func NewCaegory(name string) *Category {
+	return &Category{
+		ID:   uuid.New().String(),
+		Name: name,
+	}
+}
+
+type Product struct {
+	ID          string
+	Name        string
+	Description string
+	Price       float64
+	CategoryID  string
+	ImageURL    string
+}
+
+func NewProduct(name, description, categoryID, imageURL string, price float64) *Product {
+	return &Product{
+		ID:          uuid.New().String(),
+		Name:        name,
+		Description: description,
+		CategoryID:  categoryID,
+		ImageURL:    imageURL,
+		Price:       price,
+	}
+}
